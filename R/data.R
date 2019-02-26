1a608f8271a4d2fc51ac087d0ddf3c85cd06c2c6
jdallmann <justin@jdallmann.org>
2019-02-25 21:20:41 -0600

    Change escape sequence typo

--- R/data.R
+++ R/data.R
@@ -7,17 +7,17 @@
 #' @format A data frame with 31 columns:
 #' \describe{
 #'  \item{bicycle_X}{People of type X who travel via bicycle.}
-#'  \item{boundary_name}{\"}
-#'  \item{boundary_number}{\"}
-#'  \item{boundary_type}{\"}
+#'  \item{boundary_name}{...}
+#'  \item{boundary_number}{...}
+#'  \item{boundary_type}{...}
 #'  \item{car_X}{People of type X who travel via car.}
-#'  \item{census_year}{\".}
+#'  \item{census_year}{...}
 #'  \item{motorcycle_X}{People of type X who travel via motorcycle}
 #'  \item{other_X}{People of type X who travel via other means.}
 #'  \item{passenger_X}{People of type X who travel via passenger.}
 #'  \item{public_X}{People of type X who travel via public transport.}
 #'  \item{taxi_X}{People of type X who travel via taxi.}
-#'  \item{total}{\"}
+#'  \item{total}{...}
 #'  \item{total_X}{Total in group X.}
 #'  \item{walk_X}{People of type X who walk as means of transportation.}
 #' }
