3ebad140dbb5dda0d5ba599aa6ef8a2288247cbb
jdallmann <justin@jdallmann.org>
2019-06-29 20:30:15 -0500

    Add census 2019 commute mode dataset for Winnipeg.

--- R/data.R
+++ R/data.R
@@ -43,3 +43,43 @@
 #'  transport_mode
 #' }
 "transport_mode"
+
+
+#' Census commute data for Winnipeg CMA 2019 with sf geography at the DA level.
+#'
+#' @source Statistics Canada via (cancensus)[https://mountainmath.github.io/cancensus/articles/cancensus.html] package.
+#' \url{https://cran.r-project.org/web/packages/cancensus/index.html}
+#' @format A data frame with 26 columns:
+#' \describe{
+#'  \item{area_km}{Shape area}
+#'  \item{dwellings}{Number of dwellings}
+#'  \item{households}{Number of households}
+#'  \item{population}{Population}
+#'  \item{region}{Region name}
+#'  \item{leaving_time_total}{v_CA16_5831: Total - Time leaving for work for the employed labour force aged 15 years and over in private households with a usual place of work or no fixed workplace address - 25% sample data}
+#'  \item{leaving_time_5}{v_CA16_5834: Between 5 a.m. and 5:59 a.m.}
+#'  \item{leaving_time_6}{v_CA16_5837: Between 6 a.m. and 6:59 a.m.}
+#'  \item{leaving_time_7}{v_CA16_5840: Between 7 a.m. and 7:59 a.m.}
+#'  \item{leaving_time_8}{v_CA16_5843: Between 8 a.m. and 8:59 a.m.}
+#'  \item{leaving_time_9to1159}{v_CA16_5846: Between 9 a.m. and 11:59 a.m.}
+#'  \item{leaving_time_rest}{v_CA16_5849: Between 12 p.m. and 4:59 a.m.}
+#'  \item{commute_mode_total}{v_CA16_5792: Total - Main mode of commuting for the employed labour force aged 15 years and over in private households with a usual place of work or no fixed workplace address - 25% sample data}
+#'  \item{commute_mode_driver}{v_CA16_5795: Car, truck, van - as a driver}
+#'  \item{commute_mode_passenger}{v_CA16_5798: Car, truck, van - as a passenger}
+#'  \item{commute_mode_walked}{v_CA16_5804: Walked}
+#'  \item{commute_mode_bicycle}{v_CA16_5807: Bicycle}
+#'  \item{commute_mode_public_transit}{v_CA16_5801: Public transit}
+#'  \item{commute_mode_other}{v_CA16_5810: Other method}
+#'  \item{duration_total}{v_CA16_5813: Total - Commuting duration for the employed labour force aged 15 years and over in private households with a usual place of work or no fixed workplace address - 25% sample data}
+#'  \item{duration_lt_15}{v_CA16_5816: Less than 15 minutes}
+#'  \item{duration_15to29}{v_CA16_5819: 15 to 29 minutes}
+#'  \item{duration_30to44}{v_CA16_5822: 30 to 44 minutes}
+#'  \item{duration_45to59}{v_CA16_5825: 45 to 59 minutes}
+#'  \item{duration_60plus}{v_CA16_5828: 60 minutes and over}
+#'  \item{geometry}{sf geometry for use in sf methods of mapping}
+#' }
+#' @examples
+#' \dontrun{
+#'  commute_mode_2019
+#' }
+"commute_mode_2019"
